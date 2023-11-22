22350fe9fe8b21399438138a2b6408fb2310777e
bananapantsu88 <astorgadiego550@gmail.com>
2023-11-22 11:16:28 -0300

    initialized index

--- index.php
+++ index.php
@@ -14,17 +14,21 @@
         </script>
     </head>
     <body>
-	<main>
-        <?php
-        if (isset($_SESSION['success'])) {
-            echo '<div class="alert alert-success" role="alert">'.$_SESSION['success'].'</div>';
-            unset($_SESSION['success']);
-        }
-        if (isset($_SESSION['warning'])) {
-            echo '<div class="alert alert-warning" role="alert">'.$_SESSION['warning'].'</div>';
-            unset($_SESSION['warning']);
-        }
-        ?>
+        <!-- HACER NAVBAR -->
+	    <main>
+            <!-- HACER BANNER (NOTICIAS) -->
+            <!-- HACER FOOTER -->
+            <?php
+            if (isset($_SESSION['success'])) {
+                echo '<div class="alert alert-success" role="alert">'.$_SESSION['success'].'</div>';
+                unset($_SESSION['success']);
+            }
+            if (isset($_SESSION['warning'])) {
+                echo '<div class="alert alert-warning" role="alert">'.$_SESSION['warning'].'</div>';
+                unset($_SESSION['warning']);
+            }
+            ?>
+
         </main>
     </body>
 </html>
