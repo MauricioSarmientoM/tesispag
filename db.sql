28e6004ccba174af1f696613bff4095c7598f4e3
Celes <celestemarmar2@gmail.com>
2023-11-24 04:22:32 -0300

    database almost ready, testing stuff

--- db.sql
+++ db.sql
@@ -1,2 +1,33 @@
 CREATE DATABASE tesis;
-CREATE TABLE users (name VARCHAR(128));
+CREATE TABLE users (rut INT NOT NULL UNIQUE PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    surname VARCHAR(128) NOT NULL,
+    description VARCHAR(1000),
+    email VARCHAR(128),
+    phone INT,
+    password VARCHAR(256) NOT NULL,
+    imageURL VARCHAR(256));
+--CREATE TABLE userTesist (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, );
+--CREATE TABLE userTutor (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, );
+CREATE TABLE super (id INT AUTO_INCREMENT PRIMARY KEY,
+    rut INT UNIQUE,
+    FOREIGN KEY (rut) REFERENCES users(rut));
+CREATE TABLE contact (id INT AUTO_INCREMENT PRIMARY KEY,
+    rut INT UNIQUE,
+    subject VARCHAR(64) NOT NULL,
+    body VARCHAR(1000),
+    FOREIGN KEY (rut) REFERENCES users(rut));
+CREATE TABLE works (id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    objective VARCHAR(128),
+    area VARCHAR(128),
+    abstract VARCHAR(1000));
+CREATE TABLE events (id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(128) NOT NULL,
+    description VARCHAR(1000),
+    image VARCHAR(256),
+    publicationDate DATE NOT NULL,
+    realizationDate DATE);
+
+INSERT INTO users VALUES
+    (207515841, 'Celeste', 'Marambio', NULL, 'celestemarmar2@gmail.com', 42707297, '$2y$10$YAw0revZnPwc1qD9igTrn.Cgl4qUOVlPkelK2jAUWB3fOHxmzF2be', NULL);
--- index.php
+++ index.php
@@ -14,17 +14,17 @@
         </script>
     </head>
     <body>
-	<main>
-        <?php
-        if (isset($_SESSION['success'])) {
-            echo '<div class="alert alert-success" role="alert">'.$_SESSION['success'].'</div>';
-            unset($_SESSION['success']);
-        }
-        if (isset($_SESSION['warning'])) {
-            echo '<div class="alert alert-warning" role="alert">'.$_SESSION['warning'].'</div>';
-            unset($_SESSION['warning']);
-        }
-        ?>
+        <main>
+            <?php
+            if (isset($_SESSION['success'])) {
+                echo '<div class="alert alert-success" role="alert">'.$_SESSION['success'].'</div>';
+                unset($_SESSION['success']);
+            }
+            if (isset($_SESSION['warning'])) {
+                echo '<div class="alert alert-warning" role="alert">'.$_SESSION['warning'].'</div>';
+                unset($_SESSION['warning']);
+            }
+            ?>
         </main>
     </body>
 </html>
