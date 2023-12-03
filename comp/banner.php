19badf1f7730996acea1768f8df98d8a23eac696
bananapantsu88 <astorgadiego550@gmail.com>
2023-12-03 02:22:10 -0300

    gestor.php parcheado

--- comp/banner.php
+++ comp/banner.php
@@ -4,7 +4,7 @@
             include './backend/connection.php';
             include './backend/select.php';
             $con = conectar();
-            $amountNews = 2;
+            $amountNews = 3;
             $news = selectEvents($con, $amountNews);
             for ($counter = 0; $counter < $amountNews; $counter++) {
                 if ($counter === 0) {
@@ -35,45 +35,15 @@
         ?>
 			<div class="carousel-caption d-md-block">
 				<h1 class = "white"><?php echo $row['title']; ?></h1>
-				<p class = "white transBG"><?php echo $row['description']; ?></p>
-				<h1>Investigadores de la Universidad de Atacama Descubren Nueva Especie de Mariposa</h1>
 				<div class="row">
 					<div class="col text-start">
-						<p>En un emocionante avance científico, un equipo de investigadores de la Universidad de Atacama ha identificado y catalogado una nueva especie de mariposa en una expedición en la selva amazónica. Este descubrimiento destaca el compromiso de la universidad con la investigación biológica y la conservación de la biodiversidad.</p>
+					<p class = "white transBG"><?php echo $row['description']; ?></p>
 					</div>
 					<div class="col text-end">
 						<h2>Fecha publicar</h2>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="carousel-item">
-			<img src="./src/FotosDIICC/_ALX9336.JPG" class="d-block carousel-img" alt="...">
-			<div class="carousel-caption d-md-block">
-				<h1>Estudiantes de la Universidad de Atacama son Ganadores del Concurso Nacional de Innovación Tecnológica</h1>
-				<div class="row">
-					<div class="col text-start">
-						<p>¡Éxito estudiantil! Un grupo de estudiantes de la Universidad de Atacama ha triunfado en el Concurso Nacional de Innovación Tecnológica con su proyecto revolucionario de inteligencia artificial para asistencia médica. Este logro subraya el enfoque vanguardista de la universidad en la formación de futuros líderes en el campo de la tecnología.</p>
-					</div>
-					<div class="col text-end">
-						<h2>Fecha publicar</h2>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="carousel-item">
-            <img src="./src/FotosDIICC/_ALX9328.JPG" class="d-block carousel-img" alt="...">
-			<div class="carousel-caption d-md-block">
-				<h1>Conferencia Internacional en la Universidad de Atacama Destaca Avances en Energías Renovables</h1>
-				<div class="row">
-					<div class="col text-start">
-					<p>La Universidad de Atacama fue anfitriona de una destacada conferencia internacional sobre energías renovables, donde expertos de todo el mundo compartieron sus investigaciones más recientes. Este evento reafirma el papel de la universidad como un centro líder en la promoción de soluciones sostenibles y la colaboración global para abordar los desafíos ambientales.</p>
-					</div>
-					<div class="col text-end">
-						<h2>Fecha publicar</h2>
-					</div>
-				</div>
-            </div>
 		</div>
         <?php
 			$counter++;
--- gestor.php
+++ gestor.php
@@ -21,14 +21,42 @@
         </script>
     </head>
     <body>
+            <!-- navbar -->
+            <?php include './comp/navbar.php'; ?>
+            <!-- end of navbar -->
         <main>
-            <?php 
-                include './comp/navbar.php';
-                include './comp/alerts.php';
-            ?>
+            <!-- Alerts -->
+            <?php include './comp/alerts.php' ?>
+            <!-- end alerts -->
+
             <!-- gestorbarra -->
-            <div class="container p-5" style="background-color: white;">
-                <h1>Gestor de base de datos</h1>
+            <div class="container py-4">
+                <h1 class="text-center">Gestor de base de datos</h1>
+                <div class="row my-4 d-flex zona">
+                    <div class="col-md-1 zona-imagen">
+                        <img class="d-flex mx-auto my-4" src="src\icons\userLogo.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <a href="users.php"><h2>Gestionar Usuarios</h2></a>
+                    </div>
+                </div>
+                <div class="row mt-4 d-flex zona">
+                    <div class="col-md-1 zona-imagen">
+                        <img class="d-flex mx-auto my-4" src="src\icons\adminLogo.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <a href="super.php"><h2>Gestionar Administración</h2></a>
+                    </div>
+                </div>
+                <div class="row mt-4 d-flex zona">
+                    <div class="col-md-1 zona-imagen">
+                        <img class="d-flex mx-auto my-4" src="src\icons\workLogo.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <a href="works.php"><h2>Gestionar Trabajos</h2></a>
+                    </div>
+                </div>
+            <!--
                 <div class="foption">
                     <span>
                         <img class="icon" src="src/u.png" alt="">
@@ -46,18 +74,18 @@
                         <img class="icon" src="src/w.png" alt="">
                     </span>
                     <a href="works.php"><span>Trabajos</span></a>
+                -->
                 </div>
             </div>
-            <?php include './comp/footer.php'; ?>
+
+            <div class="fixed-bottom">
+                <?php include './comp/footer.php'; ?>
+            </div>
+
         </main>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script type = "text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var dropdownUser = new bootstrap.Dropdown(document.getElementById('dropdownUser'));
-            });
-        </script>
     </body>
 </html>
