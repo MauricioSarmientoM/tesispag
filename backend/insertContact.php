7956660c12a0dac18610b4bb7411c4f8aa35a633
AlejandroD <alejandro.diaz.21@alumnos.uda.cl>
2023-12-13 11:45:05 -0300

    modalcontacto3

--- backend/insertContact.php
+++ backend/insertContact.php
@@ -1,4 +1,9 @@
 <?php
+
+    include("connection.php");
+
+	$connection= conectar();
+
     if (isset($_POST['rut']) && isset($_POST['subject']) && isset($_POST['body']) && isset($_POST['readed'])) {
         $rut = $_POST['rut'];
         $subject = $_POST['subject'];
@@ -9,7 +14,7 @@
 		if (!$query) {
 			die("Preparation failed: " . $connection->error);
 		}
-		$query->bind_param("isss", $rut, $subject, $body, $readed);
+		$query->bind_param("issi", $rut, $subject, $body, $readed);
 		if (!$query) {
 			die("Binding parameters failed: " . $query->error);
         }
